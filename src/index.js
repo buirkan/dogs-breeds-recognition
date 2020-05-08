25055e91f47137fd42e51c0dcc320e2cd7ae5cbb
Felipe Silva <felipealvessilva64@gmail.com>
2020-05-08 14:54:09 -0300

    Starting studies 🧠

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const Brain = require('brain.js')
+const network = new Brain.NeuralNetwork();
+
+/* Training the network example */
+network.train(
+    [
+        { input: [0, 0], output: [0] },
+        { input: [0, 1], output: [1] },
+        { input: [1, 0], output: [1] },
+        { input: [1, 1], output: [0] },
+    ]
+)
+
+const networkOutput = network.run();
